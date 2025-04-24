4c9278e3f675880519a48b119942262dfef72b31
Yousra Elassa <yousra.elassa40@bcmail.cuny.edu>
2025-04-23 22:28:08 -0400

    Upgrade scene project

--- Scene Project/myscript.js
+++ Scene Project/myscript.js
@@ -1,5 +1,7 @@
 var canvas = document.getElementById("myScene");
 var context = canvas.getContext("2d");
+context.shadowColor = "grey";
+context.shadowBlur = 20;
 
 var background = new Image();
 var foreground1 = new Image();
@@ -23,9 +25,28 @@ foreground2.onload = function() {
 
 foreground3.src = "imgs/ghost.png"
 foreground3.onload = function() {
-    context.drawImage(foreground3,250,250,320,320)
+    drawGhost()
+};
+
+let ghostY = 250;
+let direction = 1;
+
+function drawGhost() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(background, 0, 0, canvas.width, canvas.height);
+    context.drawImage(foreground1, 120, 400, 150, 150);
+    context.drawImage(foreground2, 82, 175, 180, 180);
+
+    context.drawImage(foreground3, 250, ghostY, 320, 320);
+
     context.font = "20pt Georgia";
     context.fillStyle = "black";
     context.fillText("Yousra Elassa", 223, 60);
     context.fillText("Spooky Encounter", 196, 100);
-};
+
+    if (ghostY >= 255) direction = -.5;
+    if (ghostY <= 240) direction = .5;
+    ghostY += direction * 0.5;
+
+    requestAnimationFrame(drawGhost);
+}
